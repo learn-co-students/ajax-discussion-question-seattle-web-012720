5810c27c9262e3e36da23fd50a187aea8158ca85
github username <github email address>
2020-04-22 12:08:54 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,57 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  callInfo();
 });
+
+// listens for button click
+function callInfo() {
+  const button = document.querySelector('button');
+  button.addEventListener('click', function(e) {
+    fetchData();
+  })
+}
+
+// GETS data passes it to elements
+function fetchData() {
+  fetch('https://randomuser.me/api/')
+  .then(resp => {return resp.json(); })
+  .then(data => { 
+    data.results.forEach(obj => 
+      createInfo(obj) )
+  })
+}
+
+// assigns elements to appropriate info
+function createInfo(obj) {
+    const nameTag = document.getElementById('fullname')
+    nameTag.innerText = `${obj.name.first} ${obj.name.last}`
+
+    const emailTag = document.getElementById('email')
+    emailTag.innerText = obj.email
+
+    const streetTag = document.getElementById('street')
+    streetTag.innerText = `${obj.location.street.number} ${obj.location.street.name}`
+
+    const cityTag = document.getElementById('city')
+    cityTag.innerText = obj.location.city
+    
+    const stateTag = document.getElementById('state')
+    stateTag.innerText = obj.location.state
+
+    const postcodeTag = document.getElementById('postcode')
+    postcodeTag.innerText = obj.location.postcode
+
+    const phoneTag = document.getElementById('phone')
+    phoneTag.innerText = obj.phone
+
+    const cellTag = document.getElementById('cell')
+    cellTag.innerText = obj.cell
+
+    const dobTag = document.getElementById('date_of_birth')
+    dobTag.innerText = obj.dob.date
+}
+
+
+
